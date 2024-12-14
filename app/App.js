7608a0d80589b59917d73a0c41557ff600340d59
Dharanish A M <dharanish816@gmail.com>
2024-12-14 16:58:51 +0530

    Initialized app V2

--- app/App.js
+++ app/App.js
@@ -1,69 +1,21 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LandingScreen from "./screens/LandingScreen";
-import SignupScreen from "./screens/SignupScreen";
-import * as SecureStore from "expo-secure-store";
-import { useEffect, useState } from "react";
-import { ActivityIndicator, View } from "react-native";
-import { useFonts } from "expo-font";
-import colors from "./constants/Color";
-
-const Stack = createNativeStackNavigator();
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [isFirstLaunch, setIsFirstLaunch] = useState(null);
-  const [fontsLoaded] = useFonts({
-    AfacadFluxBlack: require("./assets/fonts/AfacadFlux-Black.ttf"),
-    AfacadFluxBold: require("./assets/fonts/AfacadFlux-Bold.ttf"),
-    AfacadFluxExtraBold: require("./assets/fonts/AfacadFlux-ExtraBold.ttf"),
-    AfacadFluxExtraLight: require("./assets/fonts/AfacadFlux-ExtraLight.ttf"),
-    AfacadFluxLight: require("./assets/fonts/AfacadFlux-Light.ttf"),
-    AfacadFluxMedium: require("./assets/fonts/AfacadFlux-Medium.ttf"),
-    AfacadFluxRegular: require("./assets/fonts/AfacadFlux-Regular.ttf"),
-    AfacadFluxSemiBold: require("./assets/fonts/AfacadFlux-SemiBold.ttf"),
-    AfacadFluxThin: require("./assets/fonts/AfacadFlux-Thin.ttf"),
-  });
-
-  useEffect(() => {
-    const checkFirstLaunch = async () => {
-      try {
-        const hasLaunched = await SecureStore.getItemAsync("hasLaunched");
-        if (hasLaunched === null) {
-          await SecureStore.setItemAsync("hasLaunched", "true");
-          setIsFirstLaunch(true);
-        } else {
-          setIsFirstLaunch(false);
-        }
-      } catch (error) {
-        console.error("Error checking first launch:", error);
-      }
-    };
-
-    checkFirstLaunch();
-  }, []);
-
-  if (isFirstLaunch === null || !fontsLoaded) {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <ActivityIndicator size="large" color={colors.primaryColor} />
-      </View>
-    );
-  }
-
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={isFirstLaunch ? "Landing" : "Signup"}>
-        <Stack.Screen
-          name="Landing"
-          component={LandingScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Signup"
-          component={SignupScreen}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- web/src/pages/404.jsx
+++
@@ -1,5 +0,0 @@
-function NotFound() {
-  return <div className="not-found">Not Found</div>;
-}
-
-export default NotFound;

a35c352fe381e141ecf0d0fae701206f2a07ca65
Dharanish A M <dharanish816@gmail.com>
2024-12-19 10:57:36 +0530

    RE-APP

--- app/src/hooks/useLoadFonts.js
+++ app/src/hooks/useLoadFonts.js
@@ -1,16 +1,18 @@
 import { useFonts } from "expo-font";
 
-export const useLoadFonts = () => {
+const useCustomFonts = () => {
   const [fontsLoaded] = useFonts({
-    "Afacad-Bold": require("../assets/fonts/Afacad-Bold.ttf"),
-    "Afacad-BoldItalic": require("../assets/fonts/Afacad-BoldItalic.ttf"),
-    "Afacad-Italic": require("../assets/fonts/Afacad-Italic.ttf"),
-    "Afacad-Medium": require("../assets/fonts/Afacad-Medium.ttf"),
-    "Afacad-MediumItalic": require("../assets/fonts/Afacad-MediumItalic.ttf"),
     "Afacad-Regular": require("../assets/fonts/Afacad-Regular.ttf"),
+    "Afacad-Medium": require("../assets/fonts/Afacad-Medium.ttf"),
     "Afacad-SemiBold": require("../assets/fonts/Afacad-SemiBold.ttf"),
+    "Afacad-Bold": require("../assets/fonts/Afacad-Bold.ttf"),
+    "Afacad-Italic": require("../assets/fonts/Afacad-Italic.ttf"),
+    "Afacad-MediumItalic": require("../assets/fonts/Afacad-MediumItalic.ttf"),
     "Afacad-SemiBoldItalic": require("../assets/fonts/Afacad-SemiBoldItalic.ttf"),
+    "Afacad-BoldItalic": require("../assets/fonts/Afacad-BoldItalic.ttf"),
   });
 
   return fontsLoaded;
 };
+
+export default useCustomFonts;

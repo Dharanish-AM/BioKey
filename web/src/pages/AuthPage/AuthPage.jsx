898a263997d96b067df415b20bf470a87accba6a
Dharanish A M <dharanish816@gmail.com>
2025-02-18 13:30:08 +0530

    Web - Initialization

---
+++ web/src/pages/AuthPage/AuthPage.jsx
@@ -0,0 +1,13 @@
+import "./AuthPage.css";
+
+export default function AuthPage() {
+    return (
+        <div className="authpage-container">
+            <div className="authpage-header"></div>
+            <div className="authpage-content">
+                <div className="authpage-left"></div>
+                <div className="authpage-right"></div>
+            </div>
+        </div>
+    );
+}
